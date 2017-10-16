c515d253d0b52895f81861cff171c1d5906f2f10
Unknown <yarikbur@Mail.ru>
2017-10-16 21:22:07 +0300

    fix float

--- src/ru/yarikbur/utils/Gravity.java
+++ src/ru/yarikbur/utils/Gravity.java
@@ -9,14 +9,15 @@ public class Gravity {
 		forse = 0;
 	}
 
-	public int[] gravity(int[] obj, int weight) {
+	public int[] gravity(int[] obj, float weight) {
 		int[] gr = obj;
 		gr[1] -= (forse-forse2);
-		if((forse+(9.8f*(weight/1000)))<10)
-			forse+=9.8f*(weight/1000);
-		else
-			forse=10;
-		System.out.println(gr[1] + "  " + forse + "  " + (float)(9.8f*(50/1000)));
+		forse+=9.8f*(weight/1000f);
+		if(forse-forse2==0) {
+			forse2=0;
+			forse=0;
+		}
+		System.out.println(gr[1] + "  " + forse + "  " + forse2);
 		
 		return gr;
 	}
